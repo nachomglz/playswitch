a8f1501e7ccda1d2585f905ded4a288cfefc812d
nachomglz <nachopad3@gmail.com>
2023-10-05 18:14:58 +0200

    Changed folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-
+mod spotify;
+mod apple;
 
 fn main() {
+
+    let token = spotify::get_spotify_token();
+
+    println!("the token is {} ", token);
 }
---
+++ src/spotify.rs
@@ -0,0 +1,4 @@
+
+pub fn get_spotify_token() -> String {
+    "thisismytoken".to_owned()
+}
